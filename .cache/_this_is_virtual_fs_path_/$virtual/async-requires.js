44f1621dca835f1bc5021e80cb2a9f3e31e54534
Tonye Dickson <itztonye@gmail.com>
2023-03-22 04:30:10 +0100

    removed engine node from package.json

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -1,18 +1,11 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-pixels-js": () => import("./../../../src/pages/pixels.js?export=default" /* webpackChunkName: "component---src-pages-pixels-js" */),
-  "component---src-pages-rova-js": () => import("./../../../src/pages/rova.js?export=default" /* webpackChunkName: "component---src-pages-rova-js" */),
-  "component---src-pages-writing-how-to-setup-react-project-on-a-virtual-private-server-js": () => import("./../../../src/pages/writing/how-to-setup-react-project-on-a-virtual-private-server.js?export=default" /* webpackChunkName: "component---src-pages-writing-how-to-setup-react-project-on-a-virtual-private-server-js" */),
-  "component---src-pages-writing-index-js": () => import("./../../../src/pages/writing/index.js?export=default" /* webpackChunkName: "component---src-pages-writing-index-js" */),
-  "component---src-pages-writing-window-document-not-defined-gatsby-build-js": () => import("./../../../src/pages/writing/window-document-not-defined-gatsby-build.js?export=default" /* webpackChunkName: "component---src-pages-writing-window-document-not-defined-gatsby-build-js" */)
-}
-
-
-
-exports.head = {
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */)
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-pixels-js": () => import("./../../../src/pages/pixels.js" /* webpackChunkName: "component---src-pages-pixels-js" */),
+  "component---src-pages-rova-js": () => import("./../../../src/pages/rova.js" /* webpackChunkName: "component---src-pages-rova-js" */),
+  "component---src-pages-writing-how-to-setup-react-project-on-a-virtual-private-server-js": () => import("./../../../src/pages/writing/how-to-setup-react-project-on-a-virtual-private-server.js" /* webpackChunkName: "component---src-pages-writing-how-to-setup-react-project-on-a-virtual-private-server-js" */),
+  "component---src-pages-writing-index-js": () => import("./../../../src/pages/writing/index.js" /* webpackChunkName: "component---src-pages-writing-index-js" */),
+  "component---src-pages-writing-window-document-not-defined-gatsby-build-js": () => import("./../../../src/pages/writing/window-document-not-defined-gatsby-build.js" /* webpackChunkName: "component---src-pages-writing-window-document-not-defined-gatsby-build-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/Tonye/Sites/website/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/Tonye/Sites/website/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/Tonye/Sites/website/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/Tonye/Sites/website/src/pages/index.js")),
